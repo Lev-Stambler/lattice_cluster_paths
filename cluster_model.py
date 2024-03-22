ad2e69fb42a28d1e25794480a858c4dcab4b7fa0
Lev Stambler <levstamb@gmail.com>
2024-03-22 18:30:17 -0400

    initial setup

--- cluster_model.py
+++ cluster_model.py
@@ -1,9 +1,18 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from datasets import Dataset, load_dataset
 from typing import List
 import numpy as np
-import numpy.typings as npt
+import numpy.typing as npt
 import torch
 import transformer_lens
+
+DEFAULT_DEVICE = 'cpu'
+
+
+def forward_pass(model_lens: transformer_lens.HookedTransformer, t: str, layer: str) -> npt.NDArray[np.float64]:
+    o = model_lens.run_with_cache(t)
+    print(o)
+
 
 def get_optimal_layer_kmeans(model_lens: transformer_lens.HookedTransformer, tokenizer, dataset, layer: str, N_CLUTERS_UPPERBOUND=1_000) -> List[npt.NDArray[np.float64]]:
     """
@@ -12,6 +21,7 @@ def get_optimal_layer_kmeans(model_lens: transformer_lens.HookedTransformer, tok
     """
     raise NotImplementedError
     pass
+
 
 def cluster_model_lattice(model_lens, layers_to_centroids: List[List[npt.NDArray[np.float64]]], distance_cutoff=float("inf")):
     """
@@ -23,6 +33,7 @@ def cluster_model_lattice(model_lens, layers_to_centroids: List[List[npt.NDArray
     raise NotImplementedError
     pass
 
+
 def find_max_flow():
     """
     Find the maximum flow through the lattice
@@ -32,6 +43,41 @@ def find_max_flow():
     raise NotImplementedError
     pass
 
-model_name = 'EleutherAI/pythia-70m'
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = torch.device("cpu")
+
+def get_transformer(name: str, device=DEFAULT_DEVICE):
+    """
+    Get the transformer model from the name
+    """
+    tokenizer = AutoTokenizer.from_pretrained(name)
+    model = transformer_lens.HookedTransformer.from_pretrained(name).to(device)
+    return model, tokenizer
+
+
+def get_dataset(name: str):
+    """
+    Get the dataset from the name
+    """
+    return load_dataset(name)
+
+
+def main():
+    model_name = 'EleutherAI/pythia-160m'
+    dataset_name = 'NeelNanda/pile-10k'
+
+    model, tokenizer = get_transformer(model_name)
+    ds = get_dataset(dataset_name)
+    print(ds['train'][0])
+    forward_pass(model, ds['train'][0]['text'], "")
+    return
+
+    layers_to_centroids = []
+    for layer in model_lens.layers:
+        centroids = get_optimal_layer_kmeans(model, tokenizer, dataset, layer)
+        layers_to_centroids.append(centroids)
+    lattice = cluster_model_lattice(model, layers_to_centroids)
+    max_flow = find_max_flow(lattice)
+    return max_flow
+
+
+if __name__ == '__main__':
+    main()
