6914245d69794b2903d0c3a5b1ef62b13058c1a8
Lev Stambler <levstamb@gmail.com>
2024-04-07 12:43:50 -0400

    coms

--- lattice_builder.py
+++ lattice_builder.py
@@ -10,16 +10,18 @@ class Lattice:
     _corr_cutoffs: List[npt.NDArray[1]]
     lattice: List[npt.NDArray[2]]
     
+    # TODO: variable degree!!!
     def __init__(self, correlations: List[npt.NDArray], target_degree=4) -> None:
         self._correlations = correlations
         self._target_degree = target_degree
         self._corr_cutoffs = self._get_corr_cutoff()
-        self.lattice = correlations > np.expand_dims(self._corr_cutoffs, axis=-1)
-        print(self.lattice)
+        self.lattice = [
+            corr > np.expand_dims(self._corr_cutoffs[i], axis=-1)
+        for i, corr in enumerate(correlations)]
+        print(self.lattice[0].sum(axis=-1))
     
     def _get_corr_cutoff(self):
         corrs = self._correlations
-        target = self._target_degree
         cutoffs = []
         for _, layer in enumerate(corrs):
             sorted = np.flip(np.sort(layer, axis=-1), axis=-1)
